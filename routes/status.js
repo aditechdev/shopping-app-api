14b41a70bd68abd7d06a0e9bfc2cc7072e1e7747
aditechdev <aditechdevofficial@gmail.com>
2022-06-19 19:28:41 +0530

    :sparkles: Status check

--- routes/status.js
+++ routes/status.js
@@ -7,13 +7,17 @@ statusRouter.get('/', async (req, res) => {
     
 
     try {
-        const status = await req.status;
-        if (status == 200) {
-            return res.status(200).json({
-                msg: "Its Working"
-            });
+
+        res.status(200).json({
+            "Status": "Its Working"
+        })
+        // const status = await req.status;
+        // if (status == 200) {
+        //     return res.status(200).json({
+        //         msg: "Its Working"
+        //     });
             
-        }
+        // }
         
     } catch (error) {
         res.status(500).json({ error: error.message });
