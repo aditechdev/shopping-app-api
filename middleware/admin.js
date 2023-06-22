cf57d4d1ab5e9c17aafda4eced8c6aaba000418e
Aditya Anand <aditechdevofficial@gmail.com>
2023-06-22 22:44:15 +0530

    :art: Small change

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const admin = (req, res, next) => { 
+const admin = async (req, res, next) => { 
 
     try {
         const token = req.header("x-auth-token");
